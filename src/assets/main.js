bb40818df02801713bc6d0d82fa09ef2d90f7e1b
Tsend Zandraabazar <d.se11d015@gmail.com>
2018-03-15 00:50:12 +0800

    Project number 3

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/4660814.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      addCourses(response.courses.completed);
+    }
+  });
 
-});
+  function addCourses(courses) {
+
+    var $badges = $('#$badges');
+
+    courses.forEach(function(course) {
+
+        var $course = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          'class': 'title'
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: course.url,
+          text: 'See Course'
+        }).appendTo($course);
+
+      })
+    }
